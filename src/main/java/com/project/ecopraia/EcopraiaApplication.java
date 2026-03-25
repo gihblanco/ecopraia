791ce44a1079a9c9539f8e3ba5c9bf4b7bf590c9
GIULIA MONTE BLANCO HAPPKE <giulia_happke@estudante.sesisenai.org.br>
2026-03-24 21:04:21 -0300

    feat: inicializando estrutura de projeto e pastas

---
+++ src/main/java/com/project/ecopraia/EcopraiaApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecopraia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcopraiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcopraiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ecopraia/EcopraiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecopraia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcopraiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
